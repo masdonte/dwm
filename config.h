cdc6041f1f107bc0eca36b2fe908f117687a15d4
Binette <46168797+Binetto@users.noreply.github.com>
2021-10-27 19:10:46 -0400

    Update config.h

--- config.h
+++ config.h
@@ -39,7 +39,7 @@ static Sp scratchpads[] = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "", "" };
+static const char *tags[] = { "", "", "", "", "" };
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
