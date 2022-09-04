344d2670b31a253cf4778c3014ea4cc06a3c1d81
binettexyz <46168797+binettexyz@users.noreply.github.com>
2022-09-04 14:04:24 -0400

    changed terminal

--- config.h
+++ config.h
@@ -1,8 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
 /* Constants */
-#define TERMINAL "st"
-#define TERMCLASS "st"
+#define TERMINAL "kitty"
+#define TERMCLASS "kitty"
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
