4872492dc803bea593bec0d8397e8e13ebdcd91f
Jonathan Binette <binettexyz@proton.me>
2023-03-14 22:56:34 -0400

    typo

--- config.h
+++ config.h
@@ -15,7 +15,7 @@ static const int showbar              = 1;        /* 0 means no bar */
 static const int topbar               = 1;        /* 0 means bottom bar */
 static const int barheight            = 10;       /* 0 means height will depend on font size */
 static const unsigned int colorfultag = 1;      /* 0 means use SchemeSel for selected tag */
-static const char *fonts[]     = { "sans-serif:size=12", "FontAwesome6Free=16", "materialdesignicons-webfont:size=15" };
+static const char *fonts[]     = { "sans-serif:size=12", "FontAwesome6Free:size=16", "materialdesignicons-webfont:size=15" };
 static const char dmenufont[]  = "sans-serif:size=12";
 static const char col_gray1[]  = "#222222";
 static const char col_gray2[]  = "#444444";
